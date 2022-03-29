349434eb22461b195c4d2a23efd6ffe0cdc2e575
MGPasinetti <mg.pasinetti@gmail.com>
2022-03-29 12:03:12 +0200

    searchFilm method done

--- src/components/HeaderBoolflix.vue
+++ src/components/HeaderBoolflix.vue
@@ -1,15 +1,29 @@
 <template>
   <header>
       <label for="id">TITOLO
-        <input type="text" placeholder="Cerca un film">
+        <input
+            type="text"
+            placeholder="Cerca un film"
+            v-model="filmName"
+        >
       </label>
-      <button>Search</button>
+      <button @click="searchFilm">Search</button>
   </header>
 </template>
 
 <script>
 export default {
   name: 'HeaderBoolflix',
+  data() {
+    return {
+      filmName: '',
+    };
+  },
+  methods: {
+    searchFilm() {
+      this.$emit('film-inserted', this.filmName);
+    },
+  },
 };
 </script>
 
